01b8836986b83a0d8eb8300a263f71b98e7861a8
kubilayeldemir <kubi0707@gmail.com>
2024-05-04 00:22:51 +0300

    Add more channels

--- constants.go
+++ constants.go
@@ -1,13 +1,16 @@
 package main
 
 var ChannelAndChatIdMap = map[string]int{
-	//"jahrein": 25314085,
+	"jahrein":     25314085,
 	"naru":        25540277,
 	"rraenee":     25951243,
 	"chips":       25594578,
 	"tolunayoren": 357571,
 	"kaanflix":    7437325,
 	"hype":        24495088,
+	"elwind":      25240548,
+	"elraenn":     1292179,
+	"purplebixi":  25712360,
 }
 
 var ChatIdAndChannelMap = map[int]string{}
